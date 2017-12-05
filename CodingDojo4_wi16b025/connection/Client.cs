b53f818fe91421a5394a305c54ab0d0e60dbeddc
Eric <eric_pieber@outlook.com>
2017-12-05 20:15:09 +0100

    added communication between the client and the server ( send + receive and error)

---
+++ CodingDojo4_wi16b025/connection/Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace CodingDojo4_wi16b025.connection
+{
+    class Client
+    {
+        byte[] buffer = new byte[512];
+        Socket clientSocket;
+        Action<string> MessageInformer;
+        Action AbortInformer; 
+        public Client (string ip, int port, Action<string> messageInformer, Action abortInformer) 
+        {
+            try
+            {
+                this.AbortInformer = abortInformer;
+                this.MessageInformer = messageInformer;
+                TcpClient client = new TcpClient();
+                client.Connect(IPAddress.Parse(ip), port);
+                clientSocket = client.Client;
+                StartReceiving();
+            }
+            catch (Exception)
+            {
+                messageInformer("No Connection to Server");
+                AbortInformer();
+            }
+        }
+
+        public void StartReceiving()
+        {
+            Task.Factory.StartNew(Receive);
+        }
+
+        private void Receive()
+        {
+            string message = ""; 
+            while(message != "@quit")
+            {
+                int length = clientSocket.Receive(buffer);
+                message = Encoding.UTF8.GetString(buffer, 0, length);
+
+                MessageInformer(message);
+            }
+            Close();
+        }
+
+        public void Close()
+        {
+            clientSocket.Close();
+            AbortInformer();
+        }
+
+        public void Send(string message)
+        {
+            if(clientSocket != null)
+            {
+                clientSocket.Send(Encoding.UTF8.GetBytes(message));
+            }
+        }
+    }
+}
